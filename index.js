e38819828565a72799f01e233f7175bf3cae18cc
AkshitLakhera <akshitlakhera14@gmail.com>
2026-01-31 19:40:28 +0530

    Done with the function to extract url and added into csv

--- index.js
+++ index.js
@@ -7,4 +7,38 @@ const OLD_DOMAIN = "https://old.wildwoodguitars.com"; //redirected from here
 const NEW_DOMAIN = "https://wildwoodguitars.com";
 const OUTPUT_FILE = "./urls.csv";
 //we will make stream
-const csvstream=
+const csvStream = fs.createWriteStream(OUTPUT_FILE);
+csvStream.write("old_url,new_url\n");
+//to  get all the url list
+async function getchildsitemap(){
+    const res= await axios.get(SITEMAP_INDEX);
+    const xml= await parseStringPromise(res.data);
+    return xml.sitemapindex.sitemap.map(
+        sm => sm.loc[0]
+    );
+}
+async function getUrlsFromSitemap(url) {
+    const res = await axios.get(url);
+    const xml = await parseStringPromise(res.data);
+  
+    return xml.urlset.url.map(
+      u => u.loc[0]
+    );
+  }
+  async  function run(){
+const sitemapUrls=await getchildsitemap();
+const seen = new Set();
+for(const sm of sitemapUrls){
+    const urls = await getUrlsFromSitemap(sm);
+    for (const oldUrl of urls) {
+        const newUrl = oldUrl.replace(OLD_DOMAIN, NEW_DOMAIN);
+        if (!seen.has(newUrl)) {
+          seen.add(newUrl);
+          csvStream.write(`${oldUrl},${newUrl}\n`);
+        }
+      }
+}
+csvStream.end();
+console.log(`Done. Total URLs: ${seen.size}`);
+  }  
+  run();
